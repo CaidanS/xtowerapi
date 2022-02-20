0542d1f1d6519cacde12aa186a3d2deaa352340e
Caidan Speth-McCrary <caidanspeth-mccrary@Caidans-MacBook-Pro.local>
2022-02-20 14:23:00 -0500

    Balance history table

--- main.py
+++ main.py
@@ -17,6 +17,28 @@ class Account(db.Model):
 
     def __init__(self, username):
         self.username = username
-        
+                
+        initial_balance_entry = BalanceHistory(username, 0,0,0, datetime.now())
+
         db.session.add(initial_balance_entry)
         db.session.commit()
+
+class BalanceHistory(db.Model):
+    '''
+    Table which records balance entries per username (includes transaction date and all asset balances)
+    '''
+    id = db.Column(db.Integer, primary_key=True)
+    
+    assoc_acct_id = db.Column(db.Integer, db.ForeignKey(Account.id))
+    transaction_date = db.Column(db.DateTime)
+
+    btc_balance = db.Column(db.Float)
+    usd_balance = db.Column(db.Float)
+    eth_balance = db.Column(db.Float)
+
+    def __init__(self, assoc_acct_id, btc_balance, usd_balance, eth_balance, transaction_date):
+        self.assoc_acct_id = assoc_acct_id
+        self.btc_balance = btc_balance
+        self.usd_balance = usd_balance
+        self.eth_balance = eth_balance
+        self.transaction_date = transaction_date
